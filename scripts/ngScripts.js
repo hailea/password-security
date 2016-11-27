6f946ab549a540d12f7df92caec13f3cb5d73d5a
Hailea <haileab.abrha@outlook.com>
2016-11-27 15:15:18 -0700

    Created a website for password security
    These project includes a web based training entitled information
    security and a simple app used to create a strong memorable password.

---
+++ scripts/ngScripts.js
@@ -0,0 +1,80 @@
+﻿var pwdApps = angular.module("pwdSecApp", []);/*brackets are used to declare the module*/
+
+pwdApps.controller("pwdSecurityCtrl", function ($scope) {
+    /*Select two words */
+     $scope.firstWord = "";
+     $scope.secondWord = "";
+     $scope.specialCharacters = ["!","#", "$", "%", "&","(", ")", "*", "+","-", "/", "?", "@", "]", "[", "{", "}"];
+});
+/*Create custom filters that substitute vowels*/
+pwdApps.filter('substituteVowels', function () {
+    return function (x) {
+        var i, c, txt = "";
+        for (i = 0; i < x.length; i++) {
+            c = x[i];
+            if (c == 'a') { c = $scope.aSubChars; }
+            if (c == 'e') { c = $scope.eSubChars; }
+            if (c == 'i') { c = $scope.iSubChars; }
+            if (c == 'o') { c = $scope.oSubChars; }
+            if (c == 'u') { c = $scope.uSubChars; }
+            txt += c;
+        }
+        return txt;
+    }
+    
+});
+/*Create a custom filter that capitalizes selected letters*/
+/*capitalize first letter*/
+pwdApps.filter('firstLetterUpperCase', function () {
+    return function (x) {
+        var i, c, txt = "";
+        for (i = 0; i < x.length; i++) {
+            c = x[i];
+            if (i != 0) {
+                c = c.toLowerCase();
+            }
+            if (i == 0) {
+                c = c.toUpperCase();
+            }
+            txt += c;
+        }
+        return txt;
+    };
+});
+/*Create a custom filter that capitalize second letter*/
+pwdApps.filter('secondLetterUpperCase', function () {
+    return function (x) {
+        var i, c, txt = "";
+        for (i = 0; i < x.length; i++) {
+            c = x[i];
+            if (i != 1) {
+                c = c.toLowerCase();
+            }
+            if (i == 1) {
+                c = c.toUpperCase();
+            }
+            txt += c;
+        }
+        return txt;
+    };
+});
+/*Create a custom filter that capitalize last letter*/
+pwdApps.filter('lastLetterUpperCase', function () {
+    return function (x) {
+        var i, c, txt = "";
+        for (i = 0; i < x.length; i++) {
+            c = x[i];
+            if (i != x.length - 1) {
+                c = c.toLowerCase();
+            }
+            if (i == x.length - 1) {
+                c = c.toUpperCase();
+            }
+            txt += c;
+        }
+        return txt;
+    };
+});
+
+
+/*Create a custom service that sustitutes the vowels by selected chracters (special characters)*/
---
+++ scripts/script.js
@@ -0,0 +1,54 @@
+﻿var hagScript = $.noConflict();
+hagScript(document).ready(function () {
+    /*title behaviours*/
+    hagScript("body").mouseenter(function () {
+        hagScript(".pptTitle").animate({ left: '250px' }, 'slow');
+        hagScript(".pptTitle").animate({ fontSize: 'bigger' }, 'slow');
+    });
+    /*main content behaviours*/
+    hagScript("#termsIS").click(function () {
+        hagScript("#dfnIS").fadeToggle("slow");
+    });
+    hagScript("#termsISPwd").click(function () {
+        hagScript("#dfnISPwd").fadeToggle("slow");
+    });
+    hagScript("article").mouseenter(function () {
+        hagScript(".ftrNote").fadeTo("slow", 0.9);
+    });
+    hagScript("article").mouseleave(function () {
+        hagScript(".ftrNote").fadeTo("slow", 0);
+    });
+    /*footer behaviours*/
+    hagScript(".panelPrerequiste").click(function () {
+        hagScript(".slidePrerequisite").slideToggle("slow");
+    });
+    hagScript(".PanelAttendee").click(function () {
+        hagScript(".slideAttendee").slideToggle("slow");
+    });
+    hagScript("#originalPwd").blur(function () {
+        hagScript("#testCallBack").css({ "fontSize": "200%", "background-color": "purple" }).fadeTo("slow", 0.9, function () {
+            document.getElementById("testCallBack").innerHTML = "Password Hint: " + hagScript("#originalPwd").val();
+        })
+    });
+    /*2nd Page - Practice*/
+    /*test purpose - callback function*/
+    hagScript(".substitute").click(function () {
+        hagScript("#testCallBack").fadeTo("slow", 0.9, function () {
+            document.getElementById("testCallBack").innerHTML = hagScript("#originalPwd").val();
+        })
+    });
+    /*load document from external site*/
+    hagScript("#loadSpecialChar").click(function () {
+        hagScript("#placeSpecialChar").load("../styles/SpecialChar.txt #loadSpecialCharacters", function (responseTxt, statusTxt, xhr) {
+            if (statusTxt == "success")
+                alert("External content loaded successfully!");
+            if (statusTxt == "error")
+                alert("Error: " + xhr.status + ": " + xhr.statusText);
+        });
+    });
+});
+
+
+
+
+
